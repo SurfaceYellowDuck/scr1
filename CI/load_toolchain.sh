2a92bfd7718b7e1797955bee04988f16d1b0a3f0
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2023-11-26 12:10:07 +0300

    Fix: add shellcheck of all .sh files in CI. Fix: add the missing shebang and add set -e in all .sh files. Fix: now submodules initializing only if they were not inited before. Fix: verilator installing only if it was not installed before. Fix: now toolchain not exports in path in dev version. Fix: deleted finding libs for verilator.

--- CI/load_toolchain.sh
+++ CI/load_toolchain.sh
@@ -1,3 +1,7 @@
-wget https://se.math.spbu.ru/riscv-public/sc-dt-2023.08/riscv-gcc.tar.xz
-tar -xf riscv-gcc.tar.xz
-rm riscv-gcc.tar.xz
+#!/bin/sh
+set -e
+if [ ! -e "./riscv-gcc/bin" ]; then
+	wget https://se.math.spbu.ru/riscv-public/sc-dt-2023.08/riscv-gcc.tar.xz
+	tar -xf riscv-gcc.tar.xz
+	rm riscv-gcc.tar.xz
+fi
--- CI/verilator_install.sh
+++ CI/verilator_install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
-./check_verilator_deps.sh
-git clone https://github.com/verilator/verilator
-unset VERILATOR_ROOT
-cd verilator
-git pull
-git checkout v5.014
-autoconf
-./configure
-make -j "$(nproc)" 
-sudo make install
-rm -rf verilator
+if [[ ! -x "$(command -v verilator)" ]]; then
+	git clone https://github.com/verilator/verilator
+	unset VERILATOR_ROOT
+	cd verilator
+	git pull
+	git checkout v5.014
+	autoconf
+	./configure
+	make
+	sudo make install
+	cd ..
+	rm -rf verilator
+fi
+
