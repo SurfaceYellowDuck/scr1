9796ce17afffda77b28487dd2ac651b2ca0a78ae
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2023-11-27 15:31:20 +0300

    Add builded verilator. Changed all.sh, now it is not run build a verilator.

--- CI/all.sh
+++ CI/all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-./CI/verilator_install.sh
 ./CI/load_toolchain.sh
 path=$(pwd)
 export PATH="$PATH:$path/riscv-gcc/bin"
+export PATH="$PATH:$path"
 ./CI/run_make.sh
 ./CI/run_verilator_lint.sh
 ./CI/run_lint_shell_scripts.sh
