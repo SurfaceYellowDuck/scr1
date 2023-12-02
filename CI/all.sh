f8ee6ccd661a3aee6506be094ab3935ddb0ad4c5
Nikita Lukonenko <68463602+SurfaceYellowDuck@users.noreply.github.com>
2023-12-02 18:07:25 +0300

    CI (#3)
    * Fix: changed branches definition.
    
    * Fix: changed run field in main.yml, there was an error.
    
    * Fix: fix errors related to syntax in main.yml file.
    
    * Fix: fix errors related to spaces in main.yml file.
    
    * Fix: fix error related to absence of a hyphen before run in main.yml
    
    * Fix: Deleted extra spaces and dashes.
    
    * Fix: fixed error "Permission denied".
    Error related to the lack of sudo before apt-install.
    
    * Fix: fix error related to gluing commands with a slash.
    
    * Fix: add apt update and apt upgrate.
    To fix error related to unmet dependencies.
    
    * Fix: add libunwind-dev.
    To fix error related to unmet dependencies of packages.
    
    * Fix: delete zlibc from installed libraries.
    Because this library is not necessary, and it causes an error during installation.
    
    * Fix: add nproc parameter to make in verilator_install.sh.
    
    * Add builded verilator.
    Changed all.sh, now it is not run build a verilator.
    
    * Add script to load builded verilator from disc.
    Deleted builded verilator from project because it is too big.
    
    * Fix: added missing verifier files for download
    
    * Fix: fixed load of verilator.
    Changed the unpacking location of the verilator packages.
    
    * Fix: fix error "permission denied" that arising during the building.
    
    * Fix: fix warning.
    Fixed a warning related to the absence of an empty line at the end of the file.

--- CI/all.sh
+++ CI/all.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
-./CI/verilator_install.sh
 ./CI/load_toolchain.sh
 path=$(pwd)
 export PATH="$PATH:$path/riscv-gcc/bin"
+export PATH="$PATH:$path/verilator_build"
+./CI/verilator_install_prod.sh
 ./CI/run_make.sh
 ./CI/run_verilator_lint.sh
 ./CI/run_lint_shell_scripts.sh
